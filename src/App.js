edd5591874f406eec27a1e857443ddf3f7cc230e
Alen <alechkr@gmail.com>
2021-03-12 15:55:13 +0100

    added Movie component and api key

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Movie from './components/Movie';
 
 const App = () => {
+  const movies =['1', '2', '3'];
   return (
     <div>
       <h1>MovieApp</h1>
+      {movies.map(e => <p>{e}</p>)}
+      <Movie />
     </div>
   );
 }
